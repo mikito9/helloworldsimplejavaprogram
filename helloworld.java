c30ac5bce0a70d9316bdbfb90e4c8106bb0cade7
mikito9 <112415959+mikito9@users.noreply.github.com>
2026-04-23 04:28:34 +0100

    Update greeting message and add CI processing output

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch41 team with  Bharath and team");
-      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
+      System.out.println(" CI processing .....");
     }  
 } 
